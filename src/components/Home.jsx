9fe15953dde2f73fe08cfaddb6cc7c670aa5d7dd
RAJESH NAIDU <rn8231019@gmail.com>
2025-06-13 15:35:03 +0530

    login detali

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 
-
+import { Link } from 'react-router-dom'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <h1>well come to Home Page</h1>
+      <Link to="/login">Login</Link>
     </div>
   )
 }
